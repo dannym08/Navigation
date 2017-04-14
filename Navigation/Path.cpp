964806fe653347894ed3191daa938ba2193ba988
Danny <danny@Dannys-MacBook-Pro.local>
2017-04-14 15:17:36 -0700

    test

---
+++ Navigation/Path.cpp
@@ -0,0 +1,9 @@
+//
+//  Path.cpp
+//  Navigation
+//
+//  Created by Danny  on 4/14/17.
+//  Copyright © 2017 Danny . All rights reserved.
+//
+
+#include "Path.hpp"
---
+++ Navigation/Path.hpp
@@ -0,0 +1,24 @@
+//
+//  Path.hpp
+//  Navigation
+//
+//  Created by Danny  on 4/14/17.
+//  Copyright © 2017 Danny . All rights reserved.
+//
+
+#ifndef Path_hpp
+#define Path_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Path{
+    private:
+        vector<int> path;
+        int itemCount;
+public:
+    
+};
+
+#endif /* Path_hpp */
